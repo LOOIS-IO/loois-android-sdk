8acbadbfe4e9e81df7408770610309420dfbd00c
leon <526247082@qq.com>
2018-08-15 10:09:10 +0800

    Add error code.

---
+++ library/src/main/java/org/loois/dapp/rx/ErrorCode.java
@@ -0,0 +1,26 @@
+package org.loois.dapp.rx;
+
+public class ErrorCode {
+
+    public static final int FAILURE = -1;
+
+    public static final int INVOKE_EXCEPTION = -2;
+
+    public static final int INTERNAL_SERVER_ERROR = 500;
+
+    public static final int BAD_GATE_WAY = 502;
+
+    public static final int NOT_FOUND = 404;
+
+    public static final int NETWORK_UNAVAILABLE = 499;
+
+    public static final int SOCKET_TIMEOUT = 408;
+
+    public static final int UNKNOWN_HOST = 699;
+
+    public static final int UNEXPECT_ERROR = 700;
+
+
+    public static final int INVALID_PASSWORD = 1000;
+    public static final int INVALID_WALLET_FILE = 1001;
+}
